c1f4b13a20b916829c1af746ea5d61b171d85c4e
huynn109 <huynn109@gmail.com>
2018-10-05 01:34:27 +0700

    first commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool CheckIsInsideGrid(Vector2 positon)
+    {
+        return positon.x >= 0 && positon.x < gridWidth && positon.y >= 0;
+    }
+
+    public Vector2 Round(Vector2 vector)
+    {
+        return new Vector2(Mathf.Round(vector.x), Mathf.Round(vector.y));
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkUserInput();
+    }
+
+    private void checkUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.Rotate(0, 0, -90);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (CheckIsValidPosition())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            fall = Time.time;
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach(Transform transform in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(transform.position);
+            if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
